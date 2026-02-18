08b65e53ed9c2695411542b4a82702d3f4ba39cc
GhordyVargas <copiaghordy@gmail.com>
2026-02-18 12:40:53 +0100

    Update open_and_close_page.py GHORDY

--- open_and_close_page.py
+++ open_and_close_page.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 
+URL = "https://around-v1.nm.tripleten-services.com/signin?lng=es"
+
 driver = webdriver.Chrome()
-driver.maximize_window()
-
-# Open the test stand page
-driver...
-
-# Check that /signin was added to the URL
-assert ...
-
-# Close the browser
-driver...
+try:
+    driver.get(URL)
+    assert "/signin" in driver.current_url, f"URL incorrecta: {driver.current_url}"
+finally:
+    driver.quit()
